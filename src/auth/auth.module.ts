4d6d8ac51b824d79622cf76319263ffed98c82b3
miro <amirer21@gmail.com>
2022-08-07 15:04:25 +0900

    jwt : Generate a token when logging in

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -7,8 +9,15 @@ import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserRepository])
+    TypeOrmModule.forFeature([UserRepository]),
     //TypeOrmCustomModule.forCustomRepository([UserRepository])
+    JwtModule.register({
+      secret: 'secret1223', //secret text
+      signOptions:{
+        expiresIn: 60 * 60 // 토큰 유효시간 : 1시간
+      }
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt'}) //passport 기본전략 : jwt
   ],
   controllers: [AuthController],
   providers: [AuthService]
