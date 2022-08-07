5c59fc9b7c419d15849b87f0dea88ec0b2afebb1
miro <amirer21@gmail.com>
2022-08-07 21:36:49 +0900

    return code cleanup

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -1,8 +1,20 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { User } from "./user.entity";
+import { UserInfoResponseDto } from './dto/userInfoResponse.dto';
 
 //유저 정보를 가져오기위한 데코레이션
-export const GetUser = createParamDecorator((data, ctx: ExecutionContext): User => { //ExecutionContext 타입, User entity
-    const req = ctx.switchToHttp().getRequest();
-    return req.user; //request 객체에서 user만 가져온다.
+export const GetUser = createParamDecorator((data, ctx: ExecutionContext): UserInfoResponseDto => { //ExecutionContext 타입, User entity
+    const ret = new UserInfoResponseDto();
+    
+    //return req.user; //request 객체에서 user만 가져온다.
+    try {       
+        const req = ctx.switchToHttp().getRequest(); 
+        ret.id = req.user.id;
+        ret.email = req.user.email;
+        ret.returnCode = 0;
+        ret.returnMsg = "조회완료";
+    } catch (error) {
+        return error;
+    }
+    return ret;
 })
